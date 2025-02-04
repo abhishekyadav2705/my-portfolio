44f3c77bdefe8c7116f69faaf0088b0a55263f3a
Abhishek Yadav <abhishekyadav84595@gmail.com>
2025-02-04 22:12:12 +0530

    modified experience section

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import styled, { keyframes } from "styled-components";
 import { ThemeContext } from "../context/ThemeContext";
 import {
@@ -7,8 +7,6 @@ import {
   FaMapMarkerAlt,
   FaCheckCircle,
 } from "react-icons/fa";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Section = styled.section`
   padding: 2rem 1rem;
@@ -23,10 +21,12 @@ const Container = styled.div`
 `;
 
 const Title = styled.h2`
-  font-size: 2rem;
-  margin-bottom: 1rem;
+  font-size: 2.5rem;
+  margin-bottom: 1.5rem;
   color: ${(props) => props.theme.color};
   text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
 `;
 
 const JobContainer = styled.div`
@@ -47,11 +47,10 @@ const JobHeader = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 1rem;
-  position: relative;
 `;
 
 const JobTitle = styled.h3`
-  font-size: 1.5rem;
+  font-size: 2rem;
   color: #007bff;
   margin-right: 0.5rem;
 `;
@@ -61,13 +60,8 @@ const CompanyIcon = styled(FaBuilding)`
   color: #007bff;
 `;
 
-const BlurredText = styled.span`
-  filter: blur(${(props) => (props.visible ? "0px" : "5px")});
-  transition: filter 0.3s;
-`;
-
 const Company = styled.h4`
-  font-size: 1.2rem;
+  font-size: 1.5rem;
   color: ${(props) => props.theme.color};
   margin-bottom: 0.25rem;
   display: flex;
@@ -86,7 +80,7 @@ const DatesIcon = styled(FaCalendarAlt)`
 `;
 
 const Dates = styled.p`
-  font-size: 1rem;
+  font-size: 1.2rem;
   color: ${(props) => props.theme.color};
 `;
 
@@ -96,7 +90,7 @@ const LocationIcon = styled(FaMapMarkerAlt)`
 `;
 
 const Location = styled.p`
-  font-size: 1rem;
+  font-size: 1.2rem;
   color: ${(props) => props.theme.color};
 `;
 
@@ -130,91 +124,8 @@ const CheckIcon = styled(FaCheckCircle)`
   color: #007bff;
 `;
 
-const PasswordInput = styled.input`
-  margin-top: 1rem;
-  padding: 0.5rem;
-  font-size: 1rem;
-  border: 2px solid #007bff;
-  border-radius: 5px;
-  transition: border-color 0.3s;
-
-  &:focus {
-    border-color: #0056b3;
-    outline: none;
-  }
-`;
-
-const SubmitButton = styled.button`
-  margin-top: 1rem;
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  color: #fff;
-  background-color: #007bff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s, transform 0.3s;
-
-  &:hover {
-    background-color: #0056b3;
-    transform: scale(1.05);
-  }
-`;
-
-const HintButton = styled.button`
-  margin-top: 0.5rem;
-  padding: 0.3rem 0.6rem;
-  font-size: 0.9rem;
-  color: #fff;
-  background-color: #28a745;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s, transform 0.3s;
-
-  &:hover {
-    background-color: #218838;
-    transform: scale(1.05);
-  }
-`;
-
-const Message = styled.p`
-  margin-top: 1rem;
-  color: ${(props) => props.theme.color};
-  font-size: 0.9rem;
-  text-align: right;
-`;
-
-const PasswordContainer = styled.div`
-  position: absolute;
-  right: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-`;
-
 const ExperienceSection = () => {
   const { theme } = useContext(ThemeContext);
-  const [password, setPassword] = useState("");
-  const [isAuthorized, setIsAuthorized] = useState(false);
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handlePasswordSubmit = (e) => {
-    e.preventDefault();
-    if (password === "27-05-2000") {
-      setIsAuthorized(true);
-      toast.success("Password correct! Details revealed.");
-    } else {
-      toast.error("Incorrect password! use hint button for help 😒.");
-    }
-  };
-
-  const handleHintClick = () => {
-    toast.info("It's my DOB in DD-MM-YYYY format😑😑");
-  };
 
   return (
     <Section id="experience" theme={theme}>
@@ -225,42 +136,15 @@ const ExperienceSection = () => {
           <JobHeader>
             <CompanyIcon />
             <JobTitle>Software Developer</JobTitle>
-            {!isAuthorized && (
-              <PasswordContainer>
-                <form onSubmit={handlePasswordSubmit}>
-                  <PasswordInput
-                    type="password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    placeholder="Enter password"
-                  />
-                  <SubmitButton type="submit">Submit</SubmitButton>
-                </form>
-                <Message theme={theme}>
-                  Enter the correct password to reveal the hidden details!
-                </Message>
-                <HintButton onClick={handleHintClick}>Password Hint</HintButton>
-              </PasswordContainer>
-            )}
           </JobHeader>
-          <Company theme={theme}>
-            <BlurredText visible={isAuthorized}>Infosys Ltd.</BlurredText>
-          </Company>
+          <Company theme={theme}>Infosys Ltd.</Company>
           <Info>
             <DatesIcon />
-            <Dates theme={theme}>
-              <BlurredText visible={isAuthorized}>
-                10/2022 - Present
-              </BlurredText>
-            </Dates>
+            <Dates theme={theme}>10/2022 - Present</Dates>
           </Info>
           <Info>
             <LocationIcon />
-            <Location theme={theme}>
-              <BlurredText visible={isAuthorized}>
-                Nagpur, Maharashtra
-              </BlurredText>
-            </Location>
+            <Location theme={theme}>Nagpur, Maharashtra</Location>
           </Info>
           <DescriptionList theme={theme}>
             <DescriptionItem theme={theme}>
